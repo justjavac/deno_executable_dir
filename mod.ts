9bcd7bb6f35dff44e54ff1af6174daec17affca0
justjavac <justjavac@gmail.com>
2020-08-19 21:49:16 +0800

    release v0.1.0

--- mod.ts
+++ mod.ts
@@ -1,15 +1,30 @@
-/**
- * Every exported symbol ideally should have a documentation line.
+import { join } from "https://deno.land/std@0.65.0/path/posix.ts";
+
+/** Returns the path to the user's executable directory.
  *
- * It is important that documentation is easily human readable,
- * but there is also a need to provide additional styling information to ensure
- * generated documentation is more rich text.
- * Therefore JSDoc should generally follow markdown markup to enrich the text.
- *
- * follow https://deno.land/std/style_guide.md
- *
- * @param foo - Description of non obvious parameter
+ * The returned value depends on the operating system and is either a string,
+ * containing a value from the following table, or `null`.
+ * 
+ * |Platform | Value                                                           | Example                    |
+ * | ------- | --------------------------------------------------------------- | ---------------------------|
+ * | Linux   | `XDG_BIN_HOME` or `$XDG_DATA_HOME`/../bin or `$HOME`/.local/bin | /home/justjavac/.local/bin |
+ * | macOS   | -                                                               | -                          |
+ * | Windows | -                                                               | -                          |
  */
-export default function starter(foo: string): string {
-  return foo;
+export default function executable_dir(): string | null {
+  switch (Deno.build.os) {
+    case "linux": {
+      const xdg = Deno.env.get("XDG_BIN_HOME");
+      if (xdg) return xdg;
+
+      const xdgData = Deno.env.get("XDG_DATA_HOME");
+      if (xdgData) return join(xdgData, "../bin");
+
+      const home = Deno.env.get("HOME");
+      if (home) return join(home, ".local/bin");
+      break;
+    }
+  }
+
+  return null;
 }
